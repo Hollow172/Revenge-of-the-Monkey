26994c4ae03601d20318803b330405c74132ab1e
ZaAnanasa <90453612+ZaAnanasa@users.noreply.github.com>
2022-11-08 22:35:28 +0100

    Changed waypoints system to navmesh pathfinding
    added library for extended navmesh movement

--- Assets/Scripts/Waypoints.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Waypoints : MonoBehaviour
-{
-    [SerializeField] private List<Transform> points = new List<Transform>();
-    public List<Transform> Points => points;
-}
