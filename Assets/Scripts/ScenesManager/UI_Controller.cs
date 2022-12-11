1b05fa7704d7da54aad0bd9dacc478900f75294b
Dymek <dymekszymekpl@gmail.com>
2022-12-12 00:14:57 +0100

    Added bullet trails and Scene Manager (You can cerate new levels, yay!)

---
+++ Assets/Scripts/ScenesManager/UI_Controller.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI_Controller : MonoBehaviour
+{
+    public void Play()
+    {
+        SceneController.LoadScene(1);
+    }
+
+    public void Restart()
+    {
+        SceneController.Restart();
+    }
+
+    public void NextLevel()
+    {
+        SceneController.NextLevel();
+    }
+
+    public void SceneLoad(int sceneIndex)
+    {
+        SceneController.LoadScene(sceneIndex);
+    }
+}
